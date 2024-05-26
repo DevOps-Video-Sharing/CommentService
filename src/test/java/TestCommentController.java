99b7b1112ae0a03c6f5b36e08364c5b1348b03ad
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-26 09:32:44 +0700

    chore: add unit test

--- src/test/java/TestCommentController.java
+++ src/test/java/TestCommentController.java
@@ -1,71 +1,38 @@
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.programming.streaming.controller.CommentController;
-import com.programming.streaming.model.Comment;
-import com.programming.streaming.repository.CommentRepository;
-import com.programming.streaming.service.CommentService;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(CommentController.class)
+import com.programming.streaming.model.Comment;
+
+@SpringBootTest(classes = com.programming.streaming.StreamingApplication.class)
+@AutoConfigureMockMvc
 public class TestCommentController {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private CommentRepository commentRepository;
-
-    @MockBean
-    private CommentService commentService;
-
-    @InjectMocks
-    private CommentController commentController;
-
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(commentController).build();
+    @Test
+    public void testGetServiceName() throws Exception {
+        mockMvc.perform(get("/comment/"))
+                .andExpect(status().isOk());
     }
 
-    // @Test
-    // public void testGetMethodName() throws Exception {
-    //     mockMvc.perform(get("/comment/"))
-    //             .andExpect(status().isOk());
-    // }
-
-    // @Test
-    // public void testCreateComment() throws Exception {
-    //     Comment comment = Comment.builder()
-    //             .id("1")
-    //             .text("This is a comment")
-    //             .author("Author")
-    //             .likes(0)
-    //             .dislikes(0)
-    //             .build();
-
-    //     when(commentRepository.save(comment)).thenReturn(comment);
-
-    //     ObjectMapper objectMapper = new ObjectMapper();
-    //     String commentJson = objectMapper.writeValueAsString(comment);
-
-    //     mockMvc.perform(post("/comment/upload")
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(commentJson))
-    //             .andExpect(status().isCreated());
-    // }
+    @Test
+    public void testUploadComment() throws Exception {
+        Comment comment = Comment.builder()
+                .text("text")
+                .author("author")
+                .build();
+        String json = "{ \"text\": \"text\", \"author\": \"author\" }";
+        mockMvc.perform(post("/comment/upload")
+                .contentType("application/json")
+                .content(json))
+                .andExpect(status().isOk());
+    }
 }
