e3474d68dec82edc470ca81ce95a2c229a9d9974
Tạ Đức Bảo <146197243+DucBaoUIT@users.noreply.github.com>
2024-12-07 12:00:39 +0700

    Update IP Kafka

--- src/main/java/com/programming/commentService/config/KafkaProducerConfig.java
+++ src/main/java/com/programming/commentService/config/KafkaProducerConfig.java
@@ -16,7 +16,7 @@ public class KafkaProducerConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.120.131:30392");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.120.131:31182");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
